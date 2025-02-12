2fc83481cf06d9a2850293e19298613a830902e4
SoyQuillo <sjquinteros@ufpso.edu.co>
2025-02-12 11:59:53 -0500

    Fragments

---
+++ react-app/src/App.tsx
@@ -0,0 +1,10 @@
+import Titulo from "./Titulo";
+import Card from "./components/card";
+
+
+function App() {
+
+return <Card></Card>
+}
+
+export default App;
---
+++ react-app/src/components/Card.tsx
@@ -0,0 +1,32 @@
+function Card() {
+  return (
+    <div
+      className="card"
+      style={{
+        width: "350px",
+      }}
+    >
+      <div className="card-body">
+      <CardBody />
+      </div>
+    </div>
+  );
+}
+
+export function CardBody(){
+    return(
+        <>
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">
+      Some quick example text to build on the card text and make up the bulk
+      of the card's content.
+    </p>
+    <a href="#" className="btn btn-primary">
+      Go somewhere
+    </a>
+    </>)
+}
+
+
+
+export default Card;
